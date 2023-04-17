81585c1420cf55708124cc90cfd06e08937f4d6b
Malcottan <maxtryasiy@gmail.com>
2023-04-17 11:11:28 +0300

    NowEverythingWorks

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -2,9 +2,27 @@
 using namespace std;
 
 int main(){
-    int q = 1;
-    while (q < 1000){
+    cout << 2 << endl;
+    cout << 3 << endl;
+    cout << 5 << endl;
+    cout << 7 << endl;
+    int q = 10;
+    while(q <= 1000){
+        bool isPrime = true;
+        for(int w = 2; w < q; w++){
+            if(q % w == 0){
+                isPrime = false;
+                break;
+            }
+        }
+        if(isPrime == true){
+            cout << q << endl;
+        }
+        /*
+        if((q % w) != 0){
+                cout << q << endl;
+            }
+            */
         q++;
-        cout << q << endl;
     }
 }
