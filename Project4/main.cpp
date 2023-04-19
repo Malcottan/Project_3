44379f4ad30502dce24b4b1919b5823d151a4220
Malcottan <maxtryasiy@gmail.com>
2023-04-19 11:10:52 +0300

    Numbers.. Even more numbers..

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a, k(0), x, x2;
-    cout << "a="; cin >> a;
-    x = a;
-    while(a < 0){
-        a /= 10;
-        k++;
+int main() {
+    int n, q;
+    cout << "Введіть ціле число: ";
+    cin >> n;
+    q = n;
+
+    // Розбиваємо число на цифри та виводимо їх на екран
+    cout << "Цифри числа " << n << ": ";
+    int num_digits = 0;
+    int sum_digits = 0;
+    while (n != 0) {
+        int digit = n % 10;
+        cout << digit << " ";
+        num_digits++;
+        sum_digits += digit;
+        n /= 10;
     }
-    int n[k];
-    while(x < 0){
-        k--;
-        x2++;
-        n[k] = (x % 10);
-        x /= 10;
+    cout << endl;
+
+    // Виводимо кількість цифр та суму цифр числа
+    cout << "Кількість цифр: " << num_digits << endl;
+    cout << "Сума цифр: " << sum_digits << endl;
+
+    // Змінюємо порядок цифр числа на зворотний
+    int reversed = 0;
+    q = abs(q); // забезпечуємо, що n буде додатнім
+    while (q != 0) {
+        reversed = reversed * 10 + q % 10;
+        q /= 10;
     }
-        cout << n[0] << " " << n[1] << endl;
+    cout << "Число n у зворотному порядку: " << reversed << endl;
+    return 0;
 }
